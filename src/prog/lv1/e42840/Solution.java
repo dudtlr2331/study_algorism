1ad87c2258f7781d6b11894b8f88c620bdd6e89e
Cat Hanbit <dudtlr102@naver.com>
2021-08-12 21:29:19 +0900

    모의고사

---
+++ src/prog/lv1/e42840/Solution.java
@@ -0,0 +1,16 @@
+package prog.lv1.e42840;
+
+public class Solution {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] answers = {1,2,3,4,5};
+
+        s.solution(answers);
+    }
+}
+
